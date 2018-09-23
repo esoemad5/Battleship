7d621f922142ebe2a7ef0498b32fdceea7cf8e5f
esoemad5 <esoemad5@gmail.com>
2018-09-23 12:14:13 -0500

    Declared all parameters and methods for Ship class, probably.

--- Battleship/Battleship/Ship.cs
+++ Battleship/Battleship/Ship.cs
@@ -8,6 +8,9 @@ namespace Battleship
 {
     class Ship
     {
+        bool isSunk;
+        int length;
+        int[][][] location; // 1 2D array for each square it occupies.
         // Boolean parameter for active/sunk.
         // 5 Kinds of Ships, different lengths.
         // Carrier 5, Battleship 4, Submarine 3, Destroyer 3, Patrol Boat 2 (remembered all that from childhood, wow).
@@ -21,5 +24,17 @@ namespace Battleship
          * Allow overlap, but dont allow game to start if there is overlap?
          * How to handle overlap when rotating?
          */
+        public void move(ConsoleKeyInfo direction)
+        {
+
+        }
+        public void rotate()
+        {
+
+        }
+        public void checkIfSunk()
+        {
+
+        }
     }
 }
